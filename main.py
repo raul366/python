42b27b9140ee20788a42e71797cd04ed9c97ea26
Ainur <ainur08052000@mail.ru>
2025-05-20 16:05:19 +0300

    Отправка ответа

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Response
+from fastapi.responses import PlainTextResponse, HTMLResponse, JSONResponse
  
 app = FastAPI()
  
@@ -10,8 +10,17 @@ def read_root():
 
 @app.get("/root")
 def root():
-    return {"message": "Hello METANIT.COM"}
+    data = "Hello METANIT.COM"
+    return Response(content=data, media_type="text/plain")
  
 @app.get("/about")
 def about():
     return {"message": "О сайте"}
+
+@app.get("/text", response_class = PlainTextResponse)
+def root_text():
+    return "Hello METANIT.COM"
+ 
+@app.get("/html", response_class = HTMLResponse)
+def root_html():
+    return "<h2>Hello METANIT.COM</h2>"
