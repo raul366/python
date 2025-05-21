4a50a13a7abffc26fc9cc20b376e2c2a4260d5d9
Ainur <ainur08052000@mail.ru>
2025-05-21 10:01:45 +0300

    Создание простейшего API

--- main.py
+++ main.py
@@ -1,86 +1,84 @@
-from fastapi import FastAPI, Response, Path, Query, status, Body
-from fastapi.responses import PlainTextResponse, HTMLResponse, FileResponse, RedirectResponse, PlainTextResponse
-from fastapi.staticfiles import StaticFiles
-from pydantic import BaseModel, Field
-
-class Company(BaseModel):
-    name: str
+import uuid
+from fastapi import FastAPI, Body, status
+from fastapi.responses import JSONResponse, FileResponse
  
-class Person(BaseModel):
-    name: str
-    company: Company
-
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        self.id = str(uuid.uuid4())
+ 
+# условная база данных - набор объектов Person
+people = [Person("Tom", 38), Person("Bob", 42), Person("Sam", 28)]
+ 
+# для поиска пользователя в списке people
+def find_person(id):
+   for person in people: 
+        if person.id == id:
+           return person
+   return None
+ 
 app = FastAPI()
-
+ 
 @app.get("/")
-def root():
+async def main():
     return FileResponse("public/index.html")
-
-@app.get("/root")
-def root():
-    data = "Hello METANIT.COM"
-    return Response(content=data, media_type="text/plain")
  
-@app.get("/about")
-def about():
-    return {"message": "О сайте"}
-
-@app.get("/text", response_class = PlainTextResponse)
-def root_text():
-    return "Hello METANIT.COM"
+@app.get("/api/users")
+def get_people():
+    return people
  
-@app.get("/html", response_class = HTMLResponse)
-def root_html():
-    return "<h2>Hello METANIT.COM</h2>"
-
-@app.get("/file")
-def root_file():
-    return FileResponse("public/index.html", 
-                        filename="mainpage.html", 
-                        media_type="application/octet-stream")
-
-@app.get("/users/admin")
-def admin():
-    return {"message": "Hello admin"}
+@app.get("/api/users/{id}")
+def get_person(id):
+    # получаем пользователя по id
+    person = find_person(id)
+    print(person)
+    # если не найден, отправляем статусный код и сообщение об ошибке
+    if person==None:  
+        return JSONResponse(
+                status_code=status.HTTP_404_NOT_FOUND, 
+                content={ "message": "Пользователь не найден" }
+        )
+    #если пользователь найден, отправляем его
+    return person
  
-@app.get("/users")
-def get_model(name: str = "Undefined", age: int = 18):
-    return {"user_name": name, "user_age": age}
  
-@app.get("/users/{name}")
-def users(name:str  = Path(min_length=3, max_length=20)):
-    return {"name": name}
-
-@app.get("/user")
-def users(people: list[str]  = Query()):
-    return {"people": people}
-
-@app.get("/user/{name}")
-def users(name:str  = Path(min_length=3, max_length=20), 
-            age: int = Query(ge=18, lt=111)):
-    return {"name": name, "age": age}
-
-@app.get("/notfound", status_code=status.HTTP_404_NOT_FOUND)
-def notfound():
-    return  {"message": "Resource Not Found"}
-
-@app.get("/user1/{id}", status_code=200)
-def users(response: Response, id: int = Path()):
-    if id < 1:
-        response.status_code = 400
-        return {"message": "Incorrect Data"}
-    return  {"message": f"Id = {id}"}
-
-@app.get("/old")
-def old():
-    return RedirectResponse("/new", status_code=302)
+@app.post("/api/users")
+def create_person(data  = Body()):
+    person = Person(data["name"], data["age"])
+    # добавляем объект в список people
+    people.append(person)
+    return person
  
-@app.get("/new")
-def new():
-    return PlainTextResponse("Новая страница")
-
-app.mount("/static", StaticFiles(directory="public"))
-
-@app.post("/hello")
-def hello(person: Person):
-    return {"message": f"{person.name} ({person.company.name})"}
+@app.put("/api/users")
+def edit_person(data  = Body()):
+  
+    # получаем пользователя по id
+    person = find_person(data["id"])
+    # если не найден, отправляем статусный код и сообщение об ошибке
+    if person == None: 
+        return JSONResponse(
+                status_code=status.HTTP_404_NOT_FOUND, 
+                content={ "message": "Пользователь не найден" }
+        )
+    # если пользователь найден, изменяем его данные и отправляем обратно клиенту
+    person.age = data["age"]
+    person.name = data["name"]
+    return person
+ 
+ 
+@app.delete("/api/users/{id}")
+def delete_person(id):
+    # получаем пользователя по id
+    person = find_person(id)
+  
+    # если не найден, отправляем статусный код и сообщение об ошибке
+    if person == None:
+        return JSONResponse(
+                status_code=status.HTTP_404_NOT_FOUND, 
+                content={ "message": "Пользователь не найден" }
+        )
+  
+    # если пользователь найден, удаляем его
+    people.remove(person)
+    return person
