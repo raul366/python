f8534b39c1e1b55d7ef240f3d449a52914c70972
Ainur <ainur08052000@mail.ru>
2025-05-22 11:02:44 +0300

    Доработка

--- source/scheme/handbooks.py
+++ source/scheme/handbooks.py
@@ -1,7 +1,7 @@
 import uuid
 
 class Person:
-    def __init__(self, name, age):
+    def __init__(self, name: str, age: int):
         self.name = name
         self.age = age
         self.id = str(uuid.uuid4())
